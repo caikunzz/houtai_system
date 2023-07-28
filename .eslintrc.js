9e278de18b63d7e98ea68ac5731a871fb74a855d
shangzhaozhao <3064159737@qq.com>
2023-07-28 08:31:12 +0800

    feat: 完成登陆注册布局 以及部分验证
    完成登陆注册布局 以及部分验证
    
    BREAKING CHANGE:
    完成登陆注册布局 以及部分验证

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,5 +33,6 @@ module.exports = {
     'import/extensions': 'off',
     'import/no-extraneous-dependencies': 'off',
     'unicorn/filename-case': 'off',
+    eqeqeq: 'off',
   },
 };
--- src/api/modules/login.ts
+++ src/api/modules/login.ts
@@ -3,6 +3,12 @@ import service from '@/api/http';
 const loginApi = {
   // 验证登录实例
   postVerification: (params: object) => service.post(`/login`, params),
+  RegUserinfo: (params: object) => service.get('/api/v1/users/exists_user_unique_fields', params),
+  RegLogin: (params: object) => service.post('/api/v1/users/login', params),
+  GetRegCode: (params: object) => service.post('/verification_codes', params),
 };
 
 export default loginApi;
+
+// export const RegUserinfo = async (data: { username: string }) =>
+//   (await service.get<{ code: 0 | 404; data: null; msg: string }>('/api/v1/users/exists_user_unique_fields', data)).data;
